aaa248d14c1d1e6d848992a3257312cf327b25f4
ezhang7423 <ezhang7423@student.palomar.edu>
2024-06-25 07:29:10 -0400

    add mpl bar groups

--- mpl/plot_useful_commands.py
+++ mpl/plot_useful_commands.py
@@ -24,13 +24,30 @@ plt.figure(figsize=(5.8, 3))
 x = np.linspace(1, 0.2, 32)
 mean = np.mean(x)
 std = np.std(x)
-plt.axvline(mean, color="red", label="Mean")
+plt.axvline(mean, color="red", label=f"Mean: ${mean:.2f}$")
 plt.axvline(
     mean + std,
     color="red",
     linestyle="--",
     alpha=0.8,
-    label="Mean + $1\sigma$",
+    label=f"Mean + $\sigma$: ${mean+std:.2f}$",
+)
+plt.axvline(
+    mean - std,
+    color="red",
+    linestyle="--",
+    alpha=0.8,
+    label=f"Mean - $\sigma$: ${mean-std:.2f}$",
+)
+plt.legend()
+
+# * add horizontal line
+plt.axhline(
+    0.2,
+    color="black",
+    linestyle="--",
+    alpha=0.5,
+    label="y=0.2",
 )
 
 # * correlation matrix
@@ -52,6 +69,7 @@ plt.yscale("symlog")
 
 # * Be sure to only pick integer tick locations.
 from matplotlib import ticker
+
 plt.gca().xaxis.set_major_locator(ticker.MaxNLocator(integer=True))
 
 # * Plot colorbar
@@ -59,21 +77,21 @@ plt.gca().xaxis.set_major_locator(ticker.MaxNLocator(integer=True))
 import matplotlib.cm as cm
 from matplotlib.colors import Normalize
 
-color_mapper = sns.color_palette("flare", as_cmap=True) # goes from 0 to 1
+color_mapper = sns.color_palette("flare", as_cmap=True)  # goes from 0 to 1
 norm = Normalize(0, 10)
 plot(
     plt.scatter,
     x,
     y,
-    cmap=color_mapper, #!
-    c=y, #!
-    norm=norm, #!
+    cmap=color_mapper,  #!
+    c=y,  #!
+    norm=norm,  #!
     xlabel="PC2 (Generosity)",
     ylabel="Happiness",
     title="Analysis of PC 2 effect on Happiness, 2023",
 )
 cbar = plt.colorbar(
-    cm.ScalarMappable(norm=norm, cmap=color_mapper), label="Add label here" #!
+    cm.ScalarMappable(norm=norm, cmap=color_mapper), label="Add label here"  #!
 )
 
 
@@ -90,3 +108,24 @@ plt.axline(
     linestyle="--",
 )
 plt.legend()
+
+
+# * plot mean and std of timings across different widths
+
+widths = [2, 3, 5]
+timings = np.random.rand(3, 5)
+plot(plt.plot, np.mean(timings, axis=1))
+plot(
+    plt.fill_between,
+    range(len(widths)),
+    np.mean(timings, axis=1) - np.std(timings, axis=1),
+    np.mean(timings, axis=1) + np.std(timings, axis=1),
+    alpha=0.2,
+    title="Time taken to train varying model widths over 5 seeds",
+    xlabel="Width",
+    ylabel="Avg Time (s) $\pm \sigma$",
+)
+# * show only the widths actually tested on the x ticks
+plt.xticks(range(len(widths)), widths)
+plt.show()
+
